4e8a6221121709a6513c3adcd0b40697eb9afc27
Egle Lenkaityte <egle@live.com>
2025-04-24 14:34:16 +0300

    runs

--- Test/MiniAnalyzer/python/ConfFile_cfg_weightsum.py
+++ Test/MiniAnalyzer/python/ConfFile_cfg_weightsum.py
@@ -4,7 +4,7 @@ def read_filelist(file):
     with open(file, 'r') as f:
         return [line.strip() for line in f if line.strip() and not line.startswith('#')]
 
-filelist = read_filelist("rootfilessimtwo.txt") #rootfilessimone.txt or rootfilessimtwo.txt or rootfilessimtt.txt
+filelist = read_filelist("rootfilessimtt.txt") #rootfilessimone.txt or rootfilessimtwo.txt or rootfilessimtt.txt
 
 process = cms.Process("Test")
 
