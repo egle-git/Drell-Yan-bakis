20fe8fb838a64583e8b55fbcc6052bd5f45aaa05
mambroza <marijus.ambrozas@cern.ch>
2025-05-09 11:14:51 +0300

    Cleanup and ttbar test

---
+++ Test/MiniAnalyzer/python/ConfFile_cfgTTinclusive.py
@@ -0,0 +1,42 @@
+import FWCore.ParameterSet.Config as cms
+
+def read_filelist(file):
+    with open(file, 'r') as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith('#')]
+
+filelist = read_filelist("rootfilessimtt_inclusive.txt")
+
+process = cms.Process("Test")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(filelist)
+)
+
+
+process.hltHighLevel = cms.EDFilter("HLTHighLevel",
+   TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
+   HLTPaths = cms.vstring('HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ*'),
+   eventSetupPathsKey = cms.string(''),
+   andOr = cms.bool(True),
+   throw = cms.bool(True)
+)
+
+process.weights = cms.EDAnalyzer('MiniAnalyzer_weightsum',
+                                 GenEventInfo = cms.untracked.InputTag("generator"),
+                                 mcProcess = cms.string("tt"),
+)
+
+process.demo = cms.EDAnalyzer('MiniAnalyzerSim',
+                              muons = cms.InputTag("slimmedMuons"),
+                              GenParticle = cms.untracked.InputTag("prunedGenParticles"),
+                              GenEventInfo = cms.untracked.InputTag("generator"),
+                              mcProcess = cms.string("tt"),
+)
+
+
+process.p = cms.Path(process.weights+process.hltHighLevel+process.demo)
